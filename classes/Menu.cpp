7ffa2b64936449ecd4bd7c48065f5694b13467e9
Dan Hariton <danhariton22@gmail.com>
2022-09-26 21:16:47 +0300

    class Menu update

--- classes/Menu.cpp
+++ classes/Menu.cpp
@@ -10,6 +10,7 @@ public:
         Item Breakfast = Item(50, "Mic de jun ca la Englezia");
         Item Lunch = Item(30, "Zama ca la UTM");
         Item Dinner = Item(50, "Pasta ca la Italiano");
+        
     };
     ~Menu();
 };
